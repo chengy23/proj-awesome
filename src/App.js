af5bbbbf12daf97d09e14d762a763cac946c80b3
leontaolong <longt8@uw.edu>
2016-12-03 06:10:35 -0800

    Add static homepage elements

--- src/App.js
+++ src/App.js
@@ -5,103 +5,108 @@ import { hashHistory, Link } from 'react-router';
 import firebase from 'firebase';
 
 class App extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {};
-    }
-    componentDidMount() {
-        //hook up with the current auth status of firebase
-        // firebase.auth().onAuthStateChanged((firebaseUser) => {
-        //   if (firebaseUser) {
-        //     this.setState({ userId: firebaseUser.uid });
-        hashHistory.push('/home');
-        // } else {
-        //   this.setState({ userId: null });
-        //   hashHistory.push('/login');
-        // }
-        // });
-    }
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  componentDidMount() {
+    //hook up with the current auth status of firebase
+    // firebase.auth().onAuthStateChanged((firebaseUser) => {
+    //   if (firebaseUser) {
+    //     this.setState({ userId: firebaseUser.uid });
+    hashHistory.push('/home');
+    // } else {
+    //   this.setState({ userId: null });
+    //   hashHistory.push('/login');
+    // }
+    // });
+  }
 
-    signOut() {
-        /* Sign out the user, and update the state */
-        firebase.auth().signOut();
-    }
+  signOut() {
+    /* Sign out the user, and update the state */
+    firebase.auth().signOut();
+  }
 
-    render() {
-        return (
-            <div className="App">
-                <Search />
-                <main className="container">
-                    {this.props.children}
-                </main>
-                <Footer />
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="App">
+        <Search />
+        <main className="container">
+          {this.props.children}
+        </main>
+        <Footer />
+      </div>
+    );
+  }
 }
 
 
 class Search extends Component {
-   constructor(props) {
+  constructor(props) {
     super(props);
-    this.state = {searchValue: '', searchBy: ''};
+    this.state = { searchValue: '', searchBy: '' };
     this.handleClickSearch = this.handleClickSearch.bind(this);
     this.handleChange = this.handleChange.bind(this);
     this.handleSearchProf = this.handleSearchProf.bind(this);
     this.handleSearchCors = this.handleSearchCors.bind(this);
   }
 
-   handleChange(event) {
-    event.preventDefault(); 
-    this.setState({searchValue:event.target.value});
+  handleChange(event) {
+    event.preventDefault();
+    this.setState({ searchValue: event.target.value });
   }
 
   handleClickSearch(event) {
-    event.preventDefault(); 
-    console.log("Push to: " + '/' + this.state.searchBy + '/' + this.state.searchValue);
+    event.preventDefault();
+    console.log("Route to: " + '/' + this.state.searchBy + '/' + this.state.searchValue);
     // hashHistory.push('/' + this.state.searchBy + '/' + this.state.searchValue);
   }
 
   handleSearchProf() {
-    this.setState({searchBy: "professor"});
+    this.setState({ searchBy: "professor" });
   }
 
   handleSearchCors() {
-    this.setState({searchBy: "course"});
+    this.setState({ searchBy: "course" });
   }
 
-    render() {
-        return (
-            <div className="">
-                <nav role="navigation" className="navbar navbar-inverse">
-                    <div className="navbar-header">
-                        <button type="button" data-target="#navbarCollapse" data-toggle="collapse" className="navbar-toggle collapsed">
-                            <span className="sr-only">Toggle navigation</span>
-                            <span className="icon-bar"></span>
-                            <span className="icon-bar"></span>
-                            <span className="icon-bar"></span>
-                        </button>
-                        <a href="#" className="navbar-brand">Brand</a>
-                    </div>
-                    <div id="navbarCollapse" className="collapse navbar-collapse">
-                        <ul className="nav navbar-nav">
-                            <li className="" onClick={this.handleSearchProf}><a href="#">Professor</a></li>
-                            <li className="" onClick={this.handleSearchCors}><a href="#">Course</a></li>
-                        </ul>
-                        <form role="search" className="navbar-form navbar-left">
-                            <div className="form-group">
-                                <input type="text" placeholder="Search" onChange={this.handleChange} className="form-control" />
-                                <button className="glyphicon glyphicon-search" aria-hidden="true" onClick={this.handleClickSearch}></button>
-                            </div>
-                        </form>
-                        <ul className="nav navbar-nav navbar-right">
-                            <li><a href="#">Login</a></li>
-                        </ul>
-                    </div>
-                </nav>
+  render() {
+    return (
+      <div className="">
+        <nav role="navigation" className="navbar navbar-inverse">
+          <div className="navbar-header">
+            <button type="button" data-target="#navbarCollapse" data-toggle="collapse" className="navbar-toggle">
+              <span className="sr-only">Toggle navigation</span>
+              <span className="icon-bar"></span>
+              <span className="icon-bar"></span>
+              <span className="icon-bar"></span>
+            </button>
+            <a href="#" className="navbar-brand">iVal</a>
+          </div>
+          <div id="navbarCollapse" className="collapse navbar-collapse">
+            <ul className="nav navbar-nav">
+              <li className="" onClick={this.handleSearchProf}><a href="#">Professor</a></li>
+              <li className="" onClick={this.handleSearchCors}><a href="#">Course</a></li>
+            </ul>
+            <ul className="nav navbar-nav navbar-right">
+              <li><a href="#">Login</a></li>
+            </ul>
+          </div>
+          <div className="row searchBar">
+            <div className="col-lg-6 col-lg-offset-3 col-md-6 col-md-offset-3 col-sm-6 col-sm-offset-3">
+              <div className="input-group">
+                <input type="text" className="form-control" placeholder="Search" onChange={this.handleChange} />
+                <span className="input-group-btn">
+                  <button className="btn btn-default" type="button" onClick={this.handleClickSearch} ><span className="glyphicon glyphicon-search"></span></button>
+                </span>
+              </div>
             </div>
-        )
-    }
+          </div>
+
+        </nav>
+      </div>
+    )
+  }
 
 }
 
--- src/Home.js
+++ src/Home.js
@@ -2,40 +2,54 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class Home extends React.Component {
+
   render() {
     return (
       <div>
-        <h1>Home Component</h1>
-        <Header />
-        <Showcase />
+        <h1>Warmest greeting from iVal</h1>
+        <AddContent />
+        <PopList />
+        <TopRating />
       </div>
     );
   }
 }
 
-class Header extends React.Component {
+
+class PopList extends React.Component {
   render() {
     return (
       <div>
-        <Search />
+        <div className="list-group">
+          <button type="button" className="list-group-item">INFO 343</button>
+          <button type="button" className="list-group-item">INFO 343</button>
+          <button type="button" className="list-group-item">INFO 343</button>
+          <button type="button" className="list-group-item">INFO 343</button>
+          <button type="button" className="list-group-item">INFO 343</button>
+          <button type="button" className="list-group-item">INFO 343</button>
+
+        </div>
       </div>
     );
   }
 }
 
-class Search extends React.Component {
+class AddContent extends React.Component {
   render() {
     return (
-      <div>
+      <div className="container">
+      <button type="button" className="btn btn-primary addButton">Add A Professor</button>
+      <button type="button" className="btn btn-primary addButton">Add A Course</button>
       </div>
     );
   }
 }
 
-class Showcase extends React.Component {
+class TopRating extends React.Component {
   render() {
     return (
       <div>
+
       </div>
     );
   }
