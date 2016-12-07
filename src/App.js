feda2c21f24301f15f68ed31bcbd20c12247a482
Quan <minhquan611996@gmail.com>
2016-12-06 21:22:14 -0800

    style the homepage

--- src/App.js
+++ src/App.js
@@ -3,14 +3,8 @@ import './App.css';
 import Home, { Footer } from './Home.js';
 import { hashHistory, Link } from 'react-router';
 import firebase from 'firebase';
-import Carousel from './Carousel.js';
-import './css/home.css';
-var TeamCarousel = [
-   {id: 0, src: "http://uwecocar.com/images/home/car.jpg", classname: "active", name:"First", description:"This is first"},
-   {id: 1, src: "http://uwecocar.com/images/home/car.jpg", classname: "", name:"Second", description:"This is second"},
-   {id: 2, src: "http://uwecocar.com/images/home/car.jpg", classname: "", name:"Third", description:"This is third"},
-   {id: 3, src: "http://uwecocar.com/images/home/car.jpg", classname: "", name:"Fourth", description:"This is fourth"}
- ];
+import {FormControl, FormGroup, Button} from 'react-bootstrap';
+
 class App extends Component {
   constructor(props) {
     super(props);
@@ -39,8 +33,9 @@ class App extends Component {
     return (
       <div className="App">
         <Search />
-        <Carousel data={TeamCarousel}/>
-        <main className="container">
+        <main className="">
+          <div id="space">
+          </div>
           {this.props.children}
         </main>
         <Footer />
@@ -90,49 +85,42 @@ class Search extends Component {
 
   render() {
     return (
-      <div className="">
-        <nav role="navigation" className="navbar navbar-inverse" id="navbar">
-          <div className="navbar-header">
-            <button type="button" data-target="#navbarCollapse" data-toggle="collapse" className="navbar-toggle">
-              <span className="sr-only">Toggle navigation</span>
-              <span className="icon-bar"></span>
-              <span className="icon-bar"></span>
-              <span className="icon-bar"></span>
-            </button>
-            <a href="#" className="navbar-brand">iVal</a>
-          </div>
-          <div id="navbarCollapse" className="collapse navbar-collapse">
-            <ul className="nav navbar-nav">
-              <li className="" ><a href="#">Home</a></li>
-              <li className="" ><a href="https://ischool.uw.edu/">The iSchool</a></li>
-              <li className="" ><Link to="insertClass" >Didn't Find a Class?</Link></li>
-            </ul>
-            <ul className="nav navbar-nav navbar-right">
-              {!this.state.userId && <li><Link to="/login">Login</Link></li>}
-              {this.state.userId &&  /*inline conditional rendering*/
-                <li className="logout">
-                  <button className="btn btn-warning" onClick={()=>this.signOut()}>
-                    {/* Show user name on sign out button */}
-                    Sign out { firebase.auth().currentUser.displayName }
+      <div>
+      <nav className="navbar navbar-default navbar-fixed-top topnav" role="navigation">
+          <div className="container topnav">
+              <div className="navbar-header">
+                  <button type="button" className="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                      <span className="sr-only">Toggle navigation</span>
+                      <span className="icon-bar"></span>
+                      <span className="icon-bar"></span>
+                      <span className="icon-bar"></span>
                   </button>
-                </li>
-              }
-            </ul>
+                  <a className="navbar-brand topnav" href="#">iVal</a>
+              </div>
+              <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                  <ul className="nav navbar-nav">
+                    <li><a href="#">Home</a></li>
+                    <li><Link to="insertClass" >Didn't Find a Class?</Link></li>
+                    <li id="formSearch"><form className="form-inline float-xs-right">
+                          <input className="form-control" type="text" placeholder="Search for a class" onChange={this.handleChange}/>{' '}
+                          <button className="btn btn-outline-success" type="submit" onClick={this.handleClickSearch}>Search</button>
+                        </form></li>
+                  </ul>
+                  <ul className="nav navbar-nav navbar-right">
+                      {!this.state.userId && <li><Link to="/login">Login</Link></li>}
+                      {this.state.userId &&  /*inline conditional rendering*/
+                        <li className="logout">
+                          <button className="btn btn-warning" onClick={()=>this.signOut()}>
+                            {/* Show user name on sign out button */}
+                            Sign out { firebase.auth().currentUser.displayName }
+                          </button>
+                        </li>
+                      }
+                  </ul>
+              </div>
           </div>
-          <div className="container">
-          </div>
-          <div className="row searchBar">
-            <div className="col-lg-6 col-lg-offset-3 col-md-6 col-md-offset-3 col-sm-6 col-sm-offset-3">
-              <form className="input-group">
-                <input type="text" className="form-control" placeholder="Search for a class (e.g. Info 343)" onChange={this.handleChange} />
-                <span className="input-group-btn">
-                  <button className="btn btn-default" type="submit" onClick={this.handleClickSearch} ><span className="glyphicon glyphicon-search"></span></button>
-                </span>
-              </form>
-            </div>
-          </div>
-        </nav>
-      </div>
+      </nav>
+    </div>
     )
   }
 
