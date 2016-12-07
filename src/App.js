6c00bfbabe8b0f8b26546993b4784735ce796bb8
Quan <minhquan611996@gmail.com>
2016-12-06 22:54:27 -0800

    add back accessibility

--- src/App.js
+++ src/App.js
@@ -19,12 +19,10 @@ class App extends Component {
     /* Add a listener and callback for authentication events */
     firebase.auth().onAuthStateChanged(user => {
       if(user) {
-        console.log('Auth state changed: logged in as', user.email);
         this.setState({userId:user.uid});
         hashHistory.push('/home');
       }
       else{
-        console.log('Auth state changed: logged out');
         this.setState({userId: null}); //null out the saved state
       }
     })
@@ -92,25 +90,25 @@ class Search extends Component {
       <div className="">
         <nav role="navigation" className="navbar navbar-inverse" id="navbar">
           <div className="navbar-header">
-            <button type="button" data-target="#navbarCollapse" data-toggle="collapse" className="navbar-toggle">
+            <button aria-label="for mobile use" type="button" data-target="#navbarCollapse" data-toggle="collapse" className="navbar-toggle">
               <span className="sr-only">Toggle navigation</span>
               <span className="icon-bar"></span>
               <span className="icon-bar"></span>
               <span className="icon-bar"></span>
             </button>
-            <a href="#" className="navbar-brand">iVal</a>
+            <a role="button" href="#" className="navbar-brand">iVal</a>
           </div>
           <div id="navbarCollapse" className="collapse navbar-collapse">
             <ul className="nav navbar-nav">
-              <li className="" ><a href="#">Home</a></li>
-              <li className="" ><a href="https://ischool.uw.edu/">The iSchool</a></li>
-              <li className="" ><Link to="insertClass" >Didn't Find a Class?</Link></li>
+              <li><a role="button" href="#">Home</a></li>
+              <li><a href="https://ischool.uw.edu/">The iSchool</a></li>
+              <li><Link to="insertClass" >Didn't Find a Class?</Link></li>
             </ul>
             <ul className="nav navbar-nav navbar-right">
               {!this.state.userId && <li><Link to="/login">Login</Link></li>}
               {this.state.userId &&  /*inline conditional rendering*/
                 <li className="logout">
-                  <button className="btn btn-warning" onClick={()=>this.signOut()}>
+                  <button aria-label="logout" className="btn btn-warning" onClick={()=>this.signOut()}>
                     {/* Show user name on sign out button */}
                     Sign out { firebase.auth().currentUser.displayName }
                   </button>
@@ -123,9 +121,9 @@ class Search extends Component {
           <div className="row searchBar">
             <div className="col-lg-6 col-lg-offset-3 col-md-6 col-md-offset-3 col-sm-6 col-sm-offset-3">
               <form className="input-group">
-                <input type="text" className="form-control" placeholder="Search for a class (e.g. Info 343)" onChange={this.handleChange} />
+                <input aria-label="search for class" type="text" className="form-control" placeholder="Search for a class (e.g. Info 343)" onChange={this.handleChange} />
                 <span className="input-group-btn">
-                  <button className="btn btn-default" type="submit" onClick={this.handleClickSearch} ><span className="glyphicon glyphicon-search"></span></button>
+                  <button aria-label="submit button" className="btn btn-default" type="submit" onClick={this.handleClickSearch} ><span className="glyphicon glyphicon-search"></span></button>
                 </span>
               </form>
             </div>
